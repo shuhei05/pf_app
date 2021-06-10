f230e19f18a733d78baa360a2e823b1c53cdc7eb
shuhei05 <qoo0228y@gmail.com>
2021-06-10 13:59:10 +0000

    update:admin

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   def index
     @orders = Order.all.page(params[:page]).per(8)
+    #@orders = Order.order(updated_at: :desc).page(params[:page]).per(8)
   end
 
   def confirm
