c134c471b20269262a7c33831923c0a8d31eaf62
shuhei05 <qoo0228y@gmail.com>
2021-06-24 02:41:01 +0000

    update:header

--- spec/factories/admin_notice.rb
+++ spec/factories/admin_notice.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :admin_notice do
-    association :admin
+    # association :admin
     information_title { Faker::Lorem.characters(number:5) }
     information_body { Faker::Lorem.characters(number:20) }
   end
