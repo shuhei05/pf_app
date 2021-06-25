d19042d3b302fd262b240ed2bd5a20c350dec355
shuhei05 <qoo0228y@gmail.com>
2021-06-25 07:39:47 +0000

    update:spec

---
+++ spec/models/share_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe Share,"モデルに関するテスト", type: :model do
+  it "is valid with a share_information_title and share_information"
+  it "is invalid without a share_information_title and share_information"
+end
---
+++ spec/models/user_notice_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe Product,"モデルに関するテスト", type: :model do
+  it "is valid with a information_title and information_body"
+  it "is invalid without a information_title and information_body"
+end
