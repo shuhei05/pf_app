3b94d9dd022fc76f1983bde11135df4316e79f7e
shuhei05 <qoo0228y@gmail.com>
2021-06-25 07:16:21 +0000

    update:spec

--- spec/models/admin_notice_spec.rb
+++ spec/models/admin_notice_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe AdminNotice,"モデルに関するテスト", type: :model do
   describe "実際に保存してみる" do
-  it "有効な投稿内容の場合は保存されるか" do
-      expect(FactoryBot.build(:admin_notice)).to be_valid
+  it "受注元のお知らせ作成" do
+      admin = create(:admin)
+      admin_notice = build(:admin_notice,admin_id: admin.id)
+      admin_notice.valid?
+      expect(admin_notice).to be_valid
     end
   end
   it "is valid with a information_title and information_body"
