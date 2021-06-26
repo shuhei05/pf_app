23c0d5b17d5a0a5a43d242378bc87d065596811c
shuhei05 <qoo0228y@gmail.com>
2021-06-26 04:09:53 +0000

    update:spec

--- spec/models/admin_notice_spec.rb
+++ spec/models/admin_notice_spec.rb
@@ -1,14 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe AdminNotice,"モデルに関するテスト", type: :model do
-  describe "実際に保存してみる" do
-  it "受注元のお知らせ作成" do
-      admin = create(:admin)
-      admin_notice = build(:admin_notice,admin_id: admin.id)
-      admin_notice.valid?
-      expect(admin_notice).to be_valid
-    end
-  end
+
   it "is valid with a information_title and information_body and admin_id"
   it "is invalid without a information_title and information_body and admin_id"
 end
