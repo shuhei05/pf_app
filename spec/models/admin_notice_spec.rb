a3e0c2a2b789cec0241a1378041543715b3e963b
shuhei05 <qoo0228y@gmail.com>
2021-06-26 05:23:42 +0000

    update:spec

--- spec/models/admin_notice_spec.rb
+++ spec/models/admin_notice_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe AdminNotice,"モデルに関するテスト", type: :model do
-
   it "is valid with a information_title and information_body and admin_id"
   it "is invalid without a information_title and information_body and admin_id"
 end
