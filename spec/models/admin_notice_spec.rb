1ba66b79f57a23beb6a66e854451e1ee7f9857f7
shuhei05 <qoo0228y@gmail.com>
2021-06-28 10:14:30 +0000

    update:spec

--- spec/models/admin_notice_spec.rb
+++ spec/models/admin_notice_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe AdminNotice,"モデルに関するテスト", type: :model do
         expect(admin_notice).to be_valid
 	    end
 	  end
+	   context "空白のバリデーションチェック" do
+	    it "titleのバリデーションチェック" do
+	      admin_notice = AdminNotice.new(information_title: '', information_body:'hoge')
+	      expect(admin_notice).to be_invalid
+	      expect(admin_notice.errors[:information_title]).to include("を入力してください")
+	    end
+	    it "bodyが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+	      admin_notice = AdminNotice.new(information_title: 'hoge', information_body:'')
+	      expect(admin_notice).to be_invalid
+	      expect(admin_notice.errors[:information_body]).to include("を入力してください")
+	    end
+	 end
 end
